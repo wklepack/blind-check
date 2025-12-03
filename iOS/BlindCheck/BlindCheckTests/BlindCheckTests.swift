bde08c437727edacd7f8098e2665db5a1ec8efd1
Oleksii Ratiiev <>
2025-12-03 13:31:19 +0400

    initial ios setup

---
+++ iOS/BlindCheck/BlindCheckTests/BlindCheckTests.swift
@@ -0,0 +1,17 @@
+//
+//  BlindCheckTests.swift
+//  BlindCheckTests
+//
+//  Created by Oleksii Ratiiev on 03.12.2025.
+//
+
+import Testing
+@testable import BlindCheck
+
+struct BlindCheckTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
