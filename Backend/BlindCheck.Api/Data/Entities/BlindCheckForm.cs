4c39076373257ca41b8ed2f1e2c213c2ca6c735d
Krzysztof Molik <krzysztof.molik@sci-us.com>
2025-12-03 14:13:13 +0100

    Add Decedent to the BlinkCheckForm

--- Backend/BlindCheck.Api/Data/Entities/BlindCheckForm.cs
+++ Backend/BlindCheck.Api/Data/Entities/BlindCheckForm.cs
@@ -9,6 +9,7 @@ public class BlindCheckForm
 
     public required string ContractNumber { get; init; }
     public Counselor Counselor  { get; set; } = null!;
+    public Decedent Decedent  { get; set; } = null!;
     public required List<MemorialPlacement> MarkerPlacements { get; init; }
     public required BlindCheckVerification BlindCheckVerification { get; init; }
 }
---
+++ Backend/BlindCheck.Api/Data/Entities/Decedent.cs
@@ -0,0 +1,6 @@
+namespace BlindCheck.Api.Data.Entities;
+
+public class Decedent
+{
+    public required string Name { get; init; }
+}
